ed1f65b5f0b501945906d98af6d979d3180f2ac8
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2020-09-05 11:54:51 -0300

    Mudança teste git

--- web_app/src/components/home/Home.vue
+++ web_app/src/components/home/Home.vue
@@ -21,7 +21,7 @@
       <div class="column is-4">
         <div class="field is-horizontal">
           <div class="field-label is-normal">
-            <label class="label">Idade</label>
+            <label class="label">Idadee</label>
           </div>
           <div class="field-body">
             <div class="field">
